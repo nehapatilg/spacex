a4ca8a081c08d31c221cc63eccf00a82136b1609
Neha Patil <neha@bookjane.com>
2021-08-09 19:48:26 -0400

    first commit

---
+++ cypress/integration/Capsules.js
@@ -0,0 +1,41 @@
+/* eslint-disable */
+/// <reference types="cypress" />
+
+context("Verify endpoints", () => {
+  beforeEach(() => {
+    //cy.visit('https://api.spacexdata.com/v3')
+  });
+
+  it("Verify GET all capsul", () => {
+    cy.request("https://api.spacexdata.com/v3/capsules").then((response) => {
+      expect(response).to.have.property("status", 200);
+      expect(response.body.length).to.equal(19);
+      expect(response.body.capsule_serial).to.not.be.null;
+      console.log(response.body);
+      Cypress._.each(response.body, (list) => {
+        expect(list.mission).to.not.be.null;
+});
+    });
+  });
+
+  it("Verify GET one capsul", (list) => {
+    cy.fixture('c112_details').then(function (c112_details) {
+    cy.request("https://api.spacexdata.com/v3/capsules/C112").then(
+      (response) => {
+        //const body = response.body;
+        expect(response).to.have.property("status", 200);
+        expect(response.body.capsule_serial).to.not.be.null;
+        //console.log(response.body);
+        expect(response.body).has.property("capsule_id", "dragon1");
+        expect(response.body).has.property("capsule_serial", "C112");
+        
+        c112_details.missions.forEach((mission,index)=>{
+          expect(response.body.missions[index]).has.property("name",c112_details.missions[index].name);
+
+        })
+      });
+    });
+  });
+
+  
+});
